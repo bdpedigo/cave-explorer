e9b4e3a407cfa7b462e3c540f2825cb7fae83b8e
bdpedigo <benjamindpedigo@gmail.com>
2023-11-06 12:52:29 -0800

    stash

--- experiments/skeletonize_w_features.py
+++ experiments/skeletonize_w_features.py
@@ -9,6 +9,7 @@ import os
 import matplotlib.pyplot as plt
 from pkg.edits import (
     apply_edit,
+    find_supervoxel_component,
     lazy_load_initial_network,
     lazy_load_network_edits,
 )
@@ -21,10 +22,12 @@ from pkg.morphology import (
     skeletonize_networkframe,
 )
 from pkg.plot import clean_axis
+from pkg.utils import get_level2_nodes_edges
 from tqdm.autonotebook import tqdm
 
 import caveclient as cc
 from meshparty import meshwork
+from neuropull.graph import NetworkFrame
 from pcg_skel import features
 from skeleton_plot.plot_tools import plot_mw_skel
 
@@ -45,7 +48,7 @@ os.environ["SKEDITS_USE_CLOUD"] = "True"
 os.environ["SKEDITS_RECOMPUTE"] = "False"
 
 # %%
-i = 6
+i = 7
 target_id = meta.iloc[i]["target_id"]
 root_id = nuc.loc[target_id]["pt_root_id"]
 root_id = client.chunkedgraph.get_latest_roots(root_id)[0]
@@ -67,6 +70,15 @@ for metaedit_id, metaedit in tqdm(
     apply_edit(nf, metaedit)
 
 
+nuc_supervoxel = nuc.loc[target_id, "pt_supervoxel_id"]
+nuc_nf = find_supervoxel_component(nuc_supervoxel, nf, client)
+
+
+root_nodes, root_edges = get_level2_nodes_edges(root_id, client, positions=False)
+root_nf = NetworkFrame(root_nodes, root_edges)
+
+assert root_nf == nuc_nf
+
 
 soma_point = get_soma_point(root_id, client)
 skeleton, mesh, l2dict_mesh, l2dict_r_mesh = skeletonize_networkframe(
@@ -80,101 +92,52 @@ plot_mw_skel(nrn, plot_postsyn=False, plot_presyn=False, plot_soma=True)
 
 # %%
 
-from pkg.edits.changes import get_level2_lineage_components
-from pkg.morphology.synapses import map_synapse_level2_ids
-
 pre_synapses, post_synapses = get_pre_post_synapses(root_id, client)
 
-level2_lineage_component_map = get_level2_lineage_components(networkdeltas_by_operation)
-
-# %%
-
-synapses = post_synapses
-side = "post"
-verbose = True
-
-
-# %%
-for idx, row in pre_synapses.iterrows():
-    current_l2 = row["pre_pt_current_level2_id"]
-
-    # this means that that level2 node was never part of a merge or split
-    # therefore we can safely keep the current mapping from supervoxel to level2 ID
-    if current_l2 in level2_lineage_component_map:
-        print(idx)
-        break
-
-# %%
-
-side = "pre"
-idx = 36
-supervoxel = pre_synapses.loc[idx][f"{side}_pt_supervoxel_id"]
-current_l2 = client.chunkedgraph.get_roots([supervoxel], stop_layer=2)[0]
-current_l2 in level2_lineage_component_map.index
-component = level2_lineage_component_map[current_l2]
-
-candidate_level2_ids = level2_lineage_component_map[
-    level2_lineage_component_map == component
-].index
-
-candidate_level2_ids = candidate_level2_ids[candidate_level2_ids.isin(nf.nodes.index)]
-
-winning_level2_id = None
-for candidate_level2_id in candidate_level2_ids:
-    candidate_supervoxels = client.chunkedgraph.get_children(candidate_level2_id)
-    if supervoxel in candidate_supervoxels:
-        winning_level2_id = candidate_level2_id
-        break
-
-# %%
-map_synapse_level2_ids(pre_synapses, level2_lineage_component_map, "pre", client)
-
-# %%
 pre_synapses, post_synapses = map_synapses(
     pre_synapses, post_synapses, networkdeltas_by_operation, l2dict_mesh, client
 )
 apply_synapses(nrn, pre_synapses, post_synapses)
 
 # %%
-fig, axs = plt.subplots(
-    1,
-    2,
-    figsize=(8, 5),
-    sharex=True,
-    sharey=True,
-    gridspec_kw=dict(hspace=0, wspace=0),
-)
+show_plot = False
+if show_plot:
+    fig, axs = plt.subplots(
+        1,
+        2,
+        figsize=(8, 5),
+        sharex=True,
+        sharey=True,
+        gridspec_kw=dict(hspace=0, wspace=0),
+    )
 
-plot_mw_skel(nrn, plot_postsyn=True, plot_presyn=True, plot_soma=True, ax=axs[0])
+    plot_mw_skel(nrn, plot_postsyn=True, plot_presyn=True, plot_soma=True, ax=axs[0])
 
-apply_compartments(nrn, root_id, client)
+    apply_compartments(nrn, root_id, client)
 
-plot_mw_skel(
-    nrn,
-    plot_postsyn=True,
-    plot_presyn=True,
-    plot_soma=True,
-    pull_compartment_colors=True,
-    ax=axs[1],
-)
+    plot_mw_skel(
+        nrn,
+        plot_postsyn=True,
+        plot_presyn=True,
+        plot_soma=True,
+        pull_compartment_colors=True,
+        ax=axs[1],
+    )
 
+    clean_axis(axs[0])
+    clean_axis(axs[1])
 
-clean_axis(axs[0])
-clean_axis(axs[1])
+    axs[0].autoscale()
 
-axs[0].autoscale()
+    axs[0].plot(
+        [1.1, 1.1],
+        [0.1, 0.9],
+        color="darkgrey",
+        linestyle="-",
+        linewidth=2,
+        clip_on=False,
+        transform=axs[0].transAxes,
+    )
 
-axs[0].plot(
-    [1.1, 1.1],
-    [0.1, 0.9],
-    color="darkgrey",
-    linestyle="-",
-    linewidth=2,
-    clip_on=False,
-    transform=axs[0].transAxes,
-)
-
-axs[0].set_title("Before compartment\nlabeling/masking")
-axs[1].set_title("After compartment\nlabeling/masking")
-
-# %%
+    axs[0].set_title("Before compartment\nlabeling/masking")
+    axs[1].set_title("After compartment\nlabeling/masking")
