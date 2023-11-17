ebd97f2d35060c3a0021bf59636ecc3a565e2534
Ben Pedigo <benjamindpedigo@gmail.com>
2023-11-17 12:22:33 -0800

    actual science?

--- experiments/rollback_inhibitory_merges.py
+++ experiments/rollback_inhibitory_merges.py
@@ -31,7 +31,7 @@ os.environ["SKEDITS_RECOMPUTE"] = "False"
 
 # getting a table of additional metadata for each operation
 
-root_id = query_neurons["pt_root_id"].values[0]
+root_id = query_neurons["pt_root_id"].values[1]
 
 
 (
@@ -215,17 +215,6 @@ package_state(dataframes, sb, client, shorten, return_as, ngl_url, link_text)
 
 
 # %%
-
-from pkg.utils import get_level2_nodes_edges
-
-nodes, edges = get_level2_nodes_edges(root_id, client, positions=True)
-
-# %%
-from neuropull.graph import NetworkFrame
-
-final_nf = NetworkFrame(nodes, edges)
-
-# %%
 ## TODO
 #
 # The Plan
@@ -255,3 +244,258 @@ final_nf = NetworkFrame(nodes, edges)
 #   - this would also make more sense semantically since merges that are "downstream" of
 #     a given merge (more distal from the soma) would therefore have no effect, if that
 #     upstream merge is rolled back
+
+root_id = query_neurons["pt_root_id"].values[1]
+nuc_supervoxel = nuc.loc[root_id, "pt_supervoxel_id"]
+
+(
+    networkdeltas_by_operation,
+    networkdeltas_by_metaoperation,
+) = lazy_load_network_edits(root_id, client=client)
+
+nuc_dist_threshold = 10
+n_modified_nodes_threshold = 10
+
+candidate_metaedits = []
+for metaoperation_id, networkdelta in networkdeltas_by_metaoperation.items():
+    metadata = networkdelta.metadata
+    operation_ids = metadata["operation_ids"]
+
+    has_filtered = False
+    has_soma_nuc = False
+    for operation_id in operation_ids:
+        # check if any of the operations in this metaoperation are filtered
+        is_filtered = edit_stats.loc[(root_id, operation_id), "is_filtered"]
+        if is_filtered:
+            has_filtered = True
+
+        # check if any of the operations in this metaoperation are a soma/nucleus merge
+        is_merge = edit_stats.loc[(root_id, operation_id), "is_merge"]
+        if is_merge:
+            dist_um = edit_stats.loc[
+                (root_id, operation_id), "centroid_distance_to_nuc_um"
+            ]
+            n_modified_nodes = edit_stats.loc[
+                (root_id, operation_id), "n_modified_nodes"
+            ]
+            if (
+                dist_um < nuc_dist_threshold
+                and n_modified_nodes >= n_modified_nodes_threshold
+            ):
+                has_soma_nuc = True
+                break
+
+    if has_filtered and metadata["any_merges"] and (not has_soma_nuc):
+        candidate_metaedits.append(metaoperation_id)
+
+# %%
+candidate_metaedits = pd.Series(candidate_metaedits)
+
+# %%
+
+# because for this analysis we are just interested in rolling back merges from the
+# most proofread version of this neuron, we can safely just use the synapses from the
+# latest version
+pre_synapses = client.materialize.query_table(
+    "synapses_pni_2", filter_equal_dict={"pre_pt_root_id": root_id}
+)
+pre_synapses.set_index("id", inplace=True)
+# post_synapses = client.materialize.query_table(
+#     "synapses_pni_2", filter_equal_dict={"post_pt_root_id": root_id}
+# )
+
+
+# remove autapses
+pre_synapses.query("pre_pt_root_id != post_pt_root_id", inplace=True)
+
+pre_synapses["pre_pt_current_level2_id"] = client.chunkedgraph.get_roots(
+    pre_synapses["pre_pt_supervoxel_id"], stop_layer=2
+)
+
+# %%
+from pkg.utils import get_level2_nodes_edges
+
+nodes, edges = get_level2_nodes_edges(root_id, client, positions=True)
+
+# %%
+from neuropull.graph import NetworkFrame
+
+final_nf = NetworkFrame(nodes, edges)
+
+final_nf.nodes["synapses"] = [[] for _ in range(len(nodes))]
+
+for idx, synapse in pre_synapses.iterrows():
+    final_nf.nodes.loc[synapse["pre_pt_current_level2_id"], "synapses"].append(idx)
+
+# %%
+n_synapses = 0
+for idx, row in final_nf.nodes.iterrows():
+    n_synapses += len(row["synapses"])
+
+final_nf.nodes
+
+print(n_synapses)
+# %%
+
+from tqdm.autonotebook import tqdm
+
+from pkg.edits import find_supervoxel_component, reverse_edit
+
+p_merge_rollbacks = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+n_samples = 25
+
+nfs_by_sample = {}
+for p_merge_rollback in p_merge_rollbacks:
+    if p_merge_rollback == 0.0 or p_merge_rollback == 1.0:
+        _n_samples = 1
+    else:
+        _n_samples = n_samples
+    for i in tqdm(range(_n_samples)):
+        sampled_metaedits = candidate_metaedits.sample(frac=p_merge_rollback)
+
+        nf = final_nf.copy()
+        for metaedit in sampled_metaedits:
+            networkdelta = networkdeltas_by_metaoperation[metaedit]
+            reverse_edit(nf, networkdelta)
+
+        nuc_nf = find_supervoxel_component(nuc_supervoxel, nf, client)
+        if p_merge_rollback == 0.0:
+            assert nuc_nf == final_nf
+        nfs_by_sample[(np.round(1 - p_merge_rollback, 1), i)] = nuc_nf
+
+# %%
+synapses_by_sample = {}
+for key, nf in nfs_by_sample.items():
+    all_synapses = []
+    for idx, node in nf.nodes.iterrows():
+        # TODO I think can safely ignore nodes w/o "synapses" column here since they
+        # were added, but could check this
+        if isinstance(node["synapses"], list):
+            all_synapses += node["synapses"]
+    synapses_by_sample[key] = all_synapses
+
+# %%
+all_postsynaptic_targets = np.unique(pre_synapses["post_pt_root_id"])
+connectivity_df = pd.DataFrame(
+    columns=all_postsynaptic_targets, index=synapses_by_sample.keys()
+).fillna(0)
+connectivity_df.index.names = ["p_merge", "sample"]
+
+for key, synapses in synapses_by_sample.items():
+    for synapse in synapses:
+        post_root_id = pre_synapses.loc[synapse, "post_pt_root_id"]
+        connectivity_df.loc[key, post_root_id] += 1
+
+import matplotlib.pyplot as plt
+
+# %%
+p_found = connectivity_df.sum(axis=0)
+p_found = p_found.sort_values(ascending=False)
+
+connectivity_df = connectivity_df[p_found.index]
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(15, 9))
+sns.heatmap(connectivity_df, ax=ax, cmap="Blues", xticklabels=False)
+
+
+# %%
+mtypes = client.materialize.query_view("allen_column_mtypes_v2")
+
+# %%
+mtypes["target_id"].isin(nuc["id"]).mean()
+
+# %%
+
+new_root_ids = mtypes["target_id"].map(nuc.reset_index().set_index("id")["pt_root_id"])
+mtypes["root_id"] = new_root_ids
+
+mtypes.set_index("root_id", inplace=True)
+
+# %%
+connectivity_df.columns.isin(mtypes.index).mean()
+
+# %%
+
+connectivity_df.groupby(by=mtypes["cell_type"], axis=1).sum()
+
+# %%
+p_connectivity_df = connectivity_df / connectivity_df.sum(axis=1).values[:, None]
+
+p_connectivity_df.sum(axis=1)
+
+# %%
+group_connectivity_df = connectivity_df.groupby(by=mtypes["cell_type"], axis=1).sum()
+group_connectivity_df = group_connectivity_df.reindex(
+    labels=np.unique(mtypes["cell_type"]), axis=1
+).fillna(0)
+
+group_p_connectivity_df = (
+    group_connectivity_df / group_connectivity_df.sum(axis=1).values[:, None]
+)
+
+# %%
+exc_group_p_connectivity_df = group_p_connectivity_df.drop(
+    ["DTC", "ITC", "PTC", "STC"], axis=1
+)
+exc_group_p_connectivity_df = exc_group_p_connectivity_df.sort_index(axis=1)
+
+exc_group_connectivity_df = group_connectivity_df.drop(
+    ["DTC", "ITC", "PTC", "STC"], axis=1
+)
+exc_group_connectivity_df = exc_group_connectivity_df.sort_index(axis=1)
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+sns.heatmap(exc_group_p_connectivity_df, cmap="Blues", ax=ax)
+
+# %%
+exc_group_connectivity_tidy = pd.melt(
+    exc_group_connectivity_df.reset_index(),
+    id_vars=["p_merge", "sample"],
+    value_name="n_synapses",
+)
+exc_group_p_connectivity_tidy = pd.melt(
+    exc_group_p_connectivity_df.reset_index(),
+    id_vars=["p_merge", "sample"],
+    value_name="p_synapses",
+)
+
+import seaborn.objects as so
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+fig.suptitle(
+    f"Root ID {root_id}, Motif group: {query_neurons.set_index('pt_root_id').loc[root_id, 'cell_type']}"
+)
+plot1 = (
+    so.Plot(exc_group_connectivity_tidy, x="p_merge", y="n_synapses", color="cell_type")
+    .add(so.Dots(pointsize=3, alpha=0.5), so.Jitter())
+    .add(so.Line(), so.Agg())
+    .add(so.Band(), so.Est())
+    .label(
+        x="Proportion of filtered merges used",
+        y="Number of synapses",
+        # title=f"Root ID {root_id}, Motif group: {query_neurons.set_index('pt_root_id').loc[root_id, 'cell_type']}",
+        color="Target M-type",
+    )
+    .layout(engine="tight")
+    .on(axs[0])
+    # .show()
+    .save("exc_group_connectivity_tidy.png", bbox_inches="tight")
+)
+plot2 = (
+    so.Plot(
+        exc_group_p_connectivity_tidy, x="p_merge", y="p_synapses", color="cell_type"
+    )
+    .add(so.Dots(pointsize=3, alpha=0.5), so.Jitter())
+    .add(so.Line(), so.Agg())
+    .add(so.Band(), so.Est())
+    .label(
+        x="Proportion of filtered merges used",
+        y="Proportion of known outputs",
+        # title=f"Root ID {root_id}, Motif group: {query_neurons.set_index('pt_root_id').loc[root_id, 'cell_type']}",
+        color="Target M-type",
+    )
+    .layout(engine="tight")
+    .on(axs[1])
+    # .show()
+    .save("exc_group_connectivity_tidy.png", bbox_inches="tight")
+)
