fcf937a2761a21e05329e4fd7f51472f6663cb69
Ben Pedigo <benjamindpedigo@gmail.com>
2024-05-29 15:06:01 -0700

    stash

--- sandbox/edit_stats_for_chi_cells.py
+++ sandbox/edit_stats_for_chi_cells.py
@@ -11,6 +11,7 @@ from pkg.neuronframe import load_neuronframe
 from pkg.plot import set_context
 from pkg.skeleton import extract_meshwork_node_mappings
 from pkg.utils import load_manifest
+from scipy.stats import pearsonr
 from statsmodels.stats.weightstats import DescrStatsW
 from tqdm_joblib import tqdm_joblib
 
@@ -124,6 +125,11 @@ skeleton_edges = pd.concat(
     [nf.edges for nf in skeleton_nfs.values()], ignore_index=True
 )
 
+# %%
+
+
+# %%
+
 
 ds = DescrStatsW(data=skeleton_edges["radius"], weights=skeleton_edges["length"])
 
@@ -131,7 +137,7 @@ adaptive_bins = True
 if adaptive_bins:
     bins = ds.quantile(np.linspace(0.0, 1.0, 21))
 else:
-    bins = np.linspace(50, 500, 31)
+    bins = np.linspace(100, 500, 31)
 
 # %%
 
@@ -147,7 +153,8 @@ skeleton_nodes = pd.concat([nf.nodes for nf in skeleton_nfs.values()])
 skeleton_edges = pd.concat(
     [nf.edges for nf in skeleton_nfs.values()], ignore_index=True
 )
-
+skeleton_nodes["n_merges"] = skeleton_nodes["merges"].apply(len)
+skeleton_nodes["n_splits"] = skeleton_nodes["splits"].apply(len)
 
 # %%
 with open(OUT_PATH / "simple_stats" / "skeleton_nfs.pkl", "wb") as f:
@@ -158,7 +165,7 @@ with open(OUT_PATH / "simple_stats" / "skeleton_nfs.pkl", "wb") as f:
 from sklearn.model_selection import train_test_split
 
 train_root_ids, test_root_ids = train_test_split(
-    root_index, test_size=0.2, random_state=888
+    root_index, test_size=0.2, random_state=888888
 )
 
 train_skeleton_nodes = skeleton_nodes.query("root_id in @train_root_ids")
@@ -214,6 +221,63 @@ def compute_edit_morphology_stats(skeleton_nodes, skeleton_edges, units="um"):
 
 
 # %%
+skeleton_nodes.explode("operations").groupby(["operations", "radius_bin"]).size()
+
+# %%
+bin_ops = (
+    skeleton_nodes.explode("operations").groupby(["operations", "radius_bin"]).size()
+)
+bin_ops = bin_ops[bin_ops > 0]
+unique_bin_ops = bin_ops.reset_index().drop(columns=0).groupby("operations").first()
+assert unique_bin_ops.index.is_unique
+
+unique_bin_ops
+
+# %%
+units = "um"
+rows = []
+for idx, group_data in skeleton_nodes.groupby("radius_bin", dropna=True):
+    operations = np.unique(group_data["operations"].explode().dropna())
+    splits = np.unique(group_data["splits"].explode().dropna())
+    merges = np.unique(group_data["merges"].explode().dropna())
+    rows.append(
+        {
+            "radius_bin": idx,
+            "n_nodes": len(group_data),
+            "n_operations": len(operations),
+            "n_splits": len(splits),
+            "n_merges": len(merges),
+            "radius_bin_mid": idx.mid,
+        }
+    )
+
+
+results_df = pd.DataFrame(rows).set_index("radius_bin")
+
+results_df[f"length_in_bin_{units}"] = skeleton_edges.groupby("radius_bin")[
+    "length"
+].sum()
+if units == "um":
+    results_df[f"length_in_bin_{units}"] /= 1000
+
+results_df[f"operations_per_{units}"] = (
+    results_df["n_operations"] / results_df[f"length_in_bin_{units}"]
+)
+results_df[f"splits_per_{units}"] = (
+    results_df["n_splits"] / results_df[f"length_in_bin_{units}"]
+)
+results_df[f"merges_per_{units}"] = (
+    results_df["n_merges"] / results_df[f"length_in_bin_{units}"]
+)
+
+results_df[f"{units}_per_operation"] = 1 / results_df[f"operations_per_{units}"]
+results_df[f"{units}_per_split"] = 1 / results_df[f"splits_per_{units}"]
+results_df[f"{units}_per_merge"] = 1 / results_df[f"merges_per_{units}"]
+
+results_df.reset_index(inplace=True)
+
+
+# %%
 units = "um"
 
 results_df = compute_edit_morphology_stats(
@@ -265,7 +329,7 @@ ax.set_ylabel("Proportion\nof arbor")
 
 ax = axs[1]
 
-inverse = True
+inverse = False
 if inverse:
     query = f"metric.isin(['{units}_per_operation', '{units}_per_split', '{units}_per_merge'])"
 else:
@@ -299,4 +363,54 @@ ax.get_legend().set_title("Error type")
 
 fig.suptitle("Inhibitory neurons (column)")
 
-#%%
+# %%
+
+length_in_bin_by_root = test_skeleton_edges.groupby(["root_id", "radius_bin"])[
+    "length"
+].sum()
+if units == "um":
+    length_in_bin_by_root /= 1000
+
+merges_per_length = results_df.set_index("radius_bin")["merges_per_um"]
+splits_per_length = results_df.set_index("radius_bin")["splits_per_um"]
+
+predicted_merges = (length_in_bin_by_root * merges_per_length).groupby("root_id").sum()
+predicted_splits = (length_in_bin_by_root * splits_per_length).groupby("root_id").sum()
+
+sns.histplot(predicted_merges, bins=20)
+
+# %%
+
+
+true_merges = test_skeleton_nodes.groupby("root_id")["n_merges"].sum()
+
+stat, pval = pearsonr(true_merges, predicted_merges.loc[true_merges.index])
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+sns.scatterplot(x=true_merges, y=predicted_merges, ax=ax)
+ax.text(0.05, 0.95, f"Pearson R = {stat:.2f}", transform=ax.transAxes)
+
+ax.set(xlabel="Number of merges", ylabel="Predicted number of merges")
+max_val = max(ax.get_xlim()[1], ax.get_ylim()[1])
+ax.set(xlim=(0, max_val), ylim=(0, max_val))
+ax.set_xticks([0, 100, 200, 300, 400])
+ax.set_yticks([0, 100, 200, 300, 400])
+ax.plot([0, 1000], [0, 1000], linestyle="--", color="grey")
+
+true_splits = test_skeleton_nodes.groupby("root_id")["n_splits"].sum()
+
+stat, pval = pearsonr(true_splits, predicted_splits.loc[true_splits.index])
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+sns.scatterplot(x=true_splits, y=predicted_splits, ax=ax)
+ax.text(0.05, 0.95, f"Pearson R = {stat:.2f}", transform=ax.transAxes)
+ax.set(xlabel="Number of splits", ylabel="Predicted number of splits")
+
+max_val = max(ax.get_xlim()[1], ax.get_ylim()[1])
+ax.set(xlim=(0, max_val), ylim=(0, max_val))
+ax.set_xticks([0, 100, 200, 300, 400])
+ax.set_yticks([0, 100, 200, 300, 400])
+ax.plot([0, 1000], [0, 1000], linestyle="--", color="grey")
+
+
+# %%
