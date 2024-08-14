82986ea37536bfed8ff55275dbdbe0e983d2f465
Ben Pedigo <benjamindpedigo@gmail.com>
2024-08-14 16:51:38 -0700

    stash

--- pkg/pkg/figures/figures.py
+++ pkg/pkg/figures/figures.py
@@ -1,11 +1,15 @@
 from pathlib import Path
+from typing import Union
 
 import cairosvg
 import matplotlib.pyplot as plt
 import skunk
+from matplotlib.axes import Axes
 
 
-def label_axes(axs, fontsize=30, label_pos=(0, 1)):
+def label_axes(
+    axs: Axes, fontsize: int = 30, label_pos: tuple[float, float] = (0.0, 1.0)
+) -> None:
     for label, ax in axs.items():
         ax.text(
             *label_pos,
@@ -18,21 +22,21 @@ def label_axes(axs, fontsize=30, label_pos=(0, 1)):
         ax.set(xticks=[], yticks=[])
 
 
-def format_axes(axs, panel_borders=False):
+def format_axes(axs: Axes, panel_borders: bool = False) -> None:
     for label, ax in axs.items():
         if not panel_borders:
             ax.axis("off")
 
 
 def panel_mosaic(
-    mosaic,
-    panel_mapping,
-    figsize=(10, 8),
-    fontsize=30,
-    panel_borders=False,
-    constrained_layout=True,
-    label_pos=(0, 1),
-):
+    mosaic: Union[str, list[list]],
+    panel_mapping: dict[str, str],
+    figsize: tuple = (10, 8),
+    fontsize: int = 30,
+    panel_borders: bool = False,
+    constrained_layout: bool = True,
+    label_pos: tuple = (0, 1),
+) -> str:
     fig, axs = plt.subplot_mosaic(
         mosaic=mosaic,
         figsize=figsize,
@@ -51,10 +55,6 @@ def panel_mosaic(
     for label, path in png_panel_mapping.items():
         with open(path, "rb") as f:
             img = plt.imread(f)
-
-            # box = skunk.ImageBox(label, img)
-            # axs[label].imshow(img, interpolation="none", aspect=None)
-
             # plot inside a box in matplotlib so as to not modify the current axes
             new_ax = axs[label].inset_axes([0.01, 0.01, 0.98, 0.98], zorder=-1)
             new_ax.imshow(img, interpolation="none", aspect=None)
@@ -70,16 +70,19 @@ def panel_mosaic(
 
     svg = skunk.insert(svg_panel_mapping)
 
+    # only necessary to close the figure so that it doesn't display in the notebook
+    # maybe not everyone would want this
     plt.close()
     return svg
 
 
-def write_svg(svg: str, out_path: Path, formats: tuple = ("svg", "pdf"), show=True):
+def write_svg(
+    svg: str, out_path: Union[str, Path], formats: tuple = ("svg", "pdf"), show=True
+) -> None:
     if show:
         skunk.display(svg)
 
     out_path = Path(out_path)
-    out_name = out_path.stem
 
     if "svg" in formats:
         with open(str(out_path) + ".svg", "w") as f:
